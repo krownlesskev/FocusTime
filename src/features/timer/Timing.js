5746b2af6f6de70b15d7bff1583440e5c54c2dd3
Kevin Sana <kevin.sana@outlook.com>
2022-01-23 00:54:57 -0600

    removed timing value

--- src/features/timer/Timing.js
+++ src/features/timer/Timing.js
@@ -1,29 +1,26 @@
 import React from "react"
-import {View, StyleSheet,} from "react-native"
-import {RoundedButton} from '../../components/RoundedButton'
+import { View, StyleSheet, } from "react-native"
+import { RoundedButton } from '../../components/RoundedButton'
 
-export const Timing = ({ onChangeTime}) => {
+export const Timing = ({ onChangeTime }) => {
   return (
     <>
-    <View style={styles.timingButton}>
-      <RoundedButton size={75} title="15" onPress={() => onChangeTime(15)}/>
-    </View>
-    <View style={styles.timingButton}>
-      <RoundedButton size={75} title="30" onPress={() => onChangeTime(30)}/>
-    </View>
-    <View style={styles.timingButton}>
-      <RoundedButton size={75} title="45" onPress={() => onChangeTime(45)}/>
-    </View>
-    <View style={styles.timingButton}>
-      <RoundedButton size={75} title="60" onPress={() => onChangeTime(60)}/>
-    </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="15" onPress={() => onChangeTime(15)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="30" onPress={() => onChangeTime(30)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="45" onPress={() => onChangeTime(45)} />
+      </View>
     </>
   )
 }
 
 const styles = StyleSheet.create({
   timingButton: {
-    flex:1,
+    flex: 1,
     alignItems: 'center',
   }
 })
